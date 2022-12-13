51dc545aa8c66d4fbd669a8508430a60cee03631
sergiofrancalino <sergio.francalino@tjac.jus.br>
2022-12-13 18:13:21 -0300

    serviceSchema incluiu LocalSetor e unidade

--- model/service.model.js
+++ model/service.model.js
@@ -5,13 +5,25 @@ const serviceSchema = new Schema(
     details: { type: String, required: true },
     complete: { type: Boolean, default: false },
     dateFin: { type: Date },
+
     user: { type: Schema.Types.ObjectId, ref: "User" },
     collab: [{ type: Schema.Types.ObjectId, ref: "User" }],
+
     status: {
       type: String,
       enum: ["Disponivel", "Descontinuado", "Em Aprovação", "Suspenso"],
       default: "Disponivel",
     },
+
+    // Incluído para vincular o serviço ao setor
+    localSetor: { type: String,
+      required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 40,
+      lowercase: true,
+    },
+    unidade: {type: Number, default: 0},
   },
   { timestamps: true },
 );
